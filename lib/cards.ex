a5edb3851f2058e5ad809cbdfbd1e38bbabf6c7c
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-06-27 16:39:03 +0530

    Feat: implement save and load.

--- lib/cards.ex
+++ lib/cards.ex
@@ -18,4 +18,16 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, file_name) do
+    file = :erlang.term_to_binary(deck)
+    File.write(file_name, file)
+  end
+
+  def load(file_name) do
+    case File.read(file_name) do
+      {:ok, file}       -> :erlang.binary_to_term(file)
+      {:error, _status} -> "Not found"
+    end
+  end
 end
