0cb713ad6eb4c095ad35f098f771a112b0e6c9ec
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-06-27 22:28:42 +0530

    Docs: add docs using exDoc.

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Contains methods to create a deck and perform operations on it.
+  """
+
+  @doc """
+    Creates a deck of cards from the specified cards and suits.
+  """
   def create_deck do
     cards = ["Ace", "King", "Queen", "Ten", "Nine", "Eight", "Seven"]
     suits = ["Diamonds", "Spades", "Hearts", "Clubs"]
@@ -7,25 +14,40 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Shuffle the give deck of cards.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Returns whether the deck contains the given card or not.
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
-  def deal(deck, hand_size) do
+  @doc """
+    Splits the deck into two parts of size `hand-size` and deck - `hand-size`.
+  """
+  def split(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
-  def save(deck, file_name) do
+  @doc """
+    Saves the deck to given absolute path.
+  """
+  def save(deck, path) do
     file = :erlang.term_to_binary(deck)
-    File.write(file_name, file)
+    File.write(path, file)
   end
 
-  def load(file_name) do
-    case File.read(file_name) do
+  @doc """
+    Load the file from given absolute path.
+  """
+  def load(path) do
+    case File.read(path) do
       {:ok, file}       -> :erlang.binary_to_term(file)
       {:error, _status} -> "Not found"
     end
